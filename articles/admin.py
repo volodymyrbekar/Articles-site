f9bb5b343c6eabfafb99f379be1a8a922ec47ff5
vova <vova.bekar7@gmail.com>
2023-04-14 13:00:31 +0300

    tests for new app

--- articles/admin.py
+++ articles/admin.py
@@ -7,6 +7,7 @@ from .models import Article
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'slug', 'timestamp', 'updated', 'publish']
     search_fields = ['title', 'content']
+    raw_id_fields = ['user']
 
 
 admin.site.register(Article, ArticleAdmin)
