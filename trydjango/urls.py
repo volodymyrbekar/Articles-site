2a2d2a90601866e9b919abf907a0ac047bea8152
vova <vova.bekar7@gmail.com>
2023-04-26 21:19:06 +0300

    added include to urls and app name

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from articles.views import (
     article_create_view,
@@ -29,11 +29,10 @@ from accounts.views import (
 from .views import home_view
 
 urlpatterns = [
-    path('', home_view),  # index/home
+    path('', home_view),  # index / home
+    path('pantry/recipes/', include('recipes.urls')),
+    path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
-    path('articles/', article_search_view),
-    path('articles/create/', article_create_view, name='article-create'),
-    path('articles/<slug:slug>/', article_detail_view, name='article-detail'),
     path('login/', login_view),
     path('logout/', logout_view),
     path('register/', register_view),
