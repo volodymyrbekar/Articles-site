264a1c741a8a87af4247283bb0c9f051f0d108c8
vova <vova.bekar7@gmail.com>
2023-05-12 21:21:45 +0300

    changed tamplates

--- recipes/forms.py
+++ recipes/forms.py
@@ -4,9 +4,28 @@ from .models import Recipe, RecipeIngredient
 
 
 class RecipeForm(forms.ModelForm):
+    error_css_class = 'error-field'
+    required_css_class = 'required-field'
+    name = forms.CharField(help_text='This is your help! <a href="/contact">Contact us</a>')  # input field
+    # description = forms.CharField(widget=forms.Textarea(attrs={"rows": 3}))
     class Meta:
         model = Recipe
         fields = ['name', 'description', 'directions']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # django-crispy-forms
+        for field in self.fields:
+            new_date = {
+                "placeholder": f'Recipe {str(field)}',
+                "class": 'form-control'
+            }
+            self.fields[str(field)].widget.attrs.update(new_date)
+        # self.fields['name'].label = ''
+        # self.fields['name'].widget.attrs.update({'class': 'form-control-2'})
+        self.fields['description'].widget.attrs.update({'rows': "2"})
+        self.fields['directions'].widget.attrs.update({'rows': "4"})
+
 
 
 class RecipeIngredientForm(forms.ModelForm):
